5cdb988c4d9f40de2bbd94708500465b20cf7fa6
ALI ABDALLAH <hansadie2009@gmail.com>
2019-10-14 20:31:43 +0300

    python1

---
+++ myprofilewebpage/Hello_World.py
@@ -0,0 +1,10 @@
+# the line below prints hello world
+print ("Hello world")
+
+# the line below
+# calculates total
+# and stores in totals variables
+
+total = 20 + 30 + 20
+
+print(total)
---
+++ myprofilewebpage/viriables.py
@@ -0,0 +1,4 @@
+# variables are containers for temporary storing data values for later reference
+# this memory is normally primary memory (RAM)
+
+
